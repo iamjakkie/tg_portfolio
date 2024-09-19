0c4b4105daf121136fbdbae96f6b7be4eb87b826
jakkie <jakubkielbasiewicz@gmail.com>
2024-09-19 15:28:59 +0200

    init tg structure

--- bot/handlers.py
+++ bot/handlers.py
@@ -1,11 +1,1 @@
-import asyncio
-import telebot.async_telebot
-import os
 
-from bot import handlers
-
-async def check(tb, am):
-    await asyncio.sleep(5)
-    while True:
-        try:
-            
